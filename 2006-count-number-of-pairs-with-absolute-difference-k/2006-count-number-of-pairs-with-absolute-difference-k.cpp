65bcd2a2576c0a6c9e1a4b969e6d4f560a866177
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2022-06-24 17:21:34 +0530

    Time: 14 ms (86.21%), Space: 13.6 MB (17.67%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
       
-        int x=nums.size();
-       
-       /* map<int,int>m1;
-      
-        int ans=0;
-        for(int i=0;i<=200;++i)
-        {
-          m1[i]=0;
-        
-        }
-        sort(nums.begin(),nums.end());
-        for( int i=0;i<x;++i)
-        {
-       
-         if(m1[abs(nums[i]-k)]!=0 && abs(k-nums[i])<nums[i])
-         {
-          ans+=m1[abs(nums[i]-k)];
-             
-         }
-           m1[nums[i]]++;  
-
-        }*/
+      /*  int x=nums.size();
         int sum=0;
         for(int i=0;i<x;++i)
             for(int j=i+1;j<x;++j)
                 if(abs(nums[i]-nums[j])==k)
                     sum++;
-        return sum;
+        return sum;*/
+         sort(nums.begin(), nums.end());
+        int ans=0;
+        if(nums.size()==1){
+            if(k==nums[0]) return 1;
+            else return 0;
+        }
+        
+        map<int, int> mp;
+        mp[nums[0]]++;
+        for(int i=1;i<nums.size();i++){
+            if(mp.find(nums[i]-k)!=mp.end()){
+                
+                ans+=mp[nums[i]-k];
+                mp[nums[i]]++;
+            }
+            
+         
+            else mp[nums[i]]++;
+        }
+        
+        return ans;
 }
 };
