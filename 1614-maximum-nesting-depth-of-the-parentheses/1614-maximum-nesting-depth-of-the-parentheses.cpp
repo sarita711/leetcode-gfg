804db5bdbec1fa95c1b27fb0fe6e8462713e5fb0
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2022-06-27 06:14:02 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (57.75%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+     int ans=0;
+        int sum=0;
+        for(int i=0;i<s.size();++i)
+        {
+            if(s[i]=='(')
+            {
+                sum++;
+                ans=max(ans,sum);
+            }
+            else if(s[i]==')')
+            {
+                sum--;
+                
+            }
+            
+            
+        }
+        return ans;
+    }
+};
