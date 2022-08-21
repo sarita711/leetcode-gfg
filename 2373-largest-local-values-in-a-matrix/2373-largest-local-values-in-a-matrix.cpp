08473f5d29e00faf0f8ad13ffcf793bb2ee6cc45
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2022-08-21 10:45:43 +0530

    Time: 17 ms (85.30%), Space: 11.2 MB (43.39%) - LeetHub

---
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+	int findMax(vector<vector<int>>& grid, int i, int j){
+
+		int maxi = INT_MIN;
+
+		for(int x=i ; x<i+3 ; x++){
+			for(int y=j ; y<j+3 ; y++){
+				maxi = max(maxi, grid[x][y]);
+			}
+		}
+		return maxi;
+	}
+
+
+	vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+
+		int n = grid.size();
+		vector<vector<int>> ans(n-2, vector<int>(n-2));
+
+		for(int i=0 ; i<n-2 ; i++){
+			for(int j=0 ; j<n-2 ; j++){
+				ans[i][j] = findMax(grid, i, j);
+			}
+		} 
+		return ans;
+	}
+};
