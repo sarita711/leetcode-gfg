6e36e0d6177f0e623c30caa1ae3ad15050d1fad5
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2022-08-31 16:57:15 +0530

    Time: 3 ms (37.92%), Space: 6.1 MB (38.87%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(int n,vector<int>&dp)
+    {
+
+     if(n<=1)return 1;
+        int ans=0;
+        if(dp[n]!=-1)return dp[n];
+        
+        //think i as root node
+        for(int i=1;i<=n;++i)
+        {
+
+          ans+=solve(i-1,dp)*solve(n-i,dp);
+        
+        }
+        return dp[n]=ans;
+    }
+    int numTrees(int n) {
+        
+       vector<int>dp(n+1,-1);
+        
+        return solve(n,dp);
+        
+    }
+};
