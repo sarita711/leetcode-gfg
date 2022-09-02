d311ed2d918a59943baf3991d0083e39d661668f
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2022-09-02 12:23:44 +0530

    Time: 186 ms (64.90%), Space: 10.2 MB (8.02%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-     int solve(int start,int end,vector<vector<int>>&dp)
+    /* int solve(int start,int end,vector<vector<int>>&dp)
     {
         if(start>=end)
             return 0;
@@ -16,10 +16,43 @@ public:
         return dp[start][end]=maxi;
         
         
+    }*/
+    
+    int solvetab(int n)
+    {
+         vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        
+         for(int start=n;start>=1;start--)
+            {
+                for(int end=start;end<=n;end++)
+                {
+                      if(start==end)
+                      {
+                          continue;
+                      }
+                      else{
+                          int maxi=INT_MAX;
+                          for(int i=start;i<=end;i++)
+                          {
+                maxi=min(maxi,i+max(dp[start][i-1],dp[i+1][end]));
+                          
+                          
+                          }
+                          
+                           dp[start][end]=maxi;
+                          
+                      }
+                
+                
+                }
+            }
+        return dp[1][n];
+    
     }
     int getMoneyAmount(int n) {
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-       return solve(1,n,dp);
+       //return solve(1,n,dp);
+        return solvetab(n);
         
     }
 };
