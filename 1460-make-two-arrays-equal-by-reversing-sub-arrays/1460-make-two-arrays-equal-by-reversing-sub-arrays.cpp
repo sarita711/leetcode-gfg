905e759c4dce5e750644feb95da080510ae81472
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2022-09-04 00:16:15 +0530

    Time: 20 ms (57.78%), Space: 14.1 MB (96.46%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
@@ -4,26 +4,13 @@ public:
         sort(t.begin(),t.end());
         sort(a.begin(),a.end());
         
-        map<int,int>m1;
-        map<int,int>m2;
+       
         int x=t.size();
-        for(int i=0;i<x;++i)
-        {
-            m1[t[i]]++;
-           
-        }
-        for(int i=0;i<x;++i)
-        {
-            m2[a[i]]++;
-           
-        }
+       
         
         for(int i=0;i<x;++i)
         {
-            if( m1[a[i]]==0 || m2[t[i]]==0 )
-            {
-                return false;
-            }
+           
             if(a[i]!=t[i]){return false;}
             
             
