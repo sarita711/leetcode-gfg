c24a0ef7242e725322b11284611d4ba2312b4ffb
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2022-09-20 07:31:42 +0530

    Time: 7 ms (8.00%), Space: 5.7 MB (94.73%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int ans=0;
+        int k=n;
+        while(true)
+        {
+            if(n%2==0 && n%k==0)
+            {
+                return n;
+            }
+            n++;
+        }
+        return -1;
+    }
+};
