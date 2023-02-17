033229eaa43cc5b3e5352c33293c5db0d5794b37
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2023-02-17 15:12:12 +0530

    Time: 620 ms (8.04%), Space: 5.9 MB (65.56%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countTriples(int n) {
+       
+        int ans1=0;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                for(int k=1;k<=n;k++){
+                if(i*i+j*j==k*k)
+                {
+                   
+                    ans1++;
+                    
+                }
+                }
+                
+            }
+        }
+        return ans1;
+        
+    }
+};
