c386295de0979449d037e46ef5f7d9634af10ace
MS.SARITA JAKHAR <85009209+sarita711@users.noreply.github.com>
2023-03-27 19:34:12 +0530

    Time: 38 ms (16.98%), Space: 22.4 MB (57.66%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -3,37 +3,24 @@ public:
     bool validMountainArray(vector<int>& arr) {
         int x =arr.size();
         if(x<3)return false;
-        vector<int>v1;
-        vector<int>v2;
-        vector<int>v3;
-        vector<int>v4;
+        
        
         int i=1;
-        while(arr[i]>=arr[i-1] && i<x)
+        while(i<x && arr[i]>=arr[i-1])
         {
             if(arr[i]==arr[i-1])return false;
-            v1.push_back(arr[i-1]);
+            
             i++;
         }
-        if(i==x ||v1.size()==0)return false;
+        if(i==x || i==1)return false;
       
       
-        while(i<x)
+        while(i<x && arr[i]<=arr[i-1] )
         {   if(arr[i]==arr[i-1])return false;
-            v2.push_back(arr[i]);
+          
             i++;
         }
-        v3=v1;
-        v4=v2;
-        sort(v3.begin(),v3.end());
-        sort(v4.begin(),v4.end());
-        
-        reverse(v4.begin(),v4.end());
-        
-        if(v1==v3 && v4==v2)
-        {
-            return true;
-        }
-        return false;
+       if(i!=x)return false;
+        return true;
     }
 };
